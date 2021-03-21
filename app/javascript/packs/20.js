8baadf19e03b1b80643c778fe9b6249748c2dddb
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-03-21 20:17:30 +0100

    20 complete

--- app/javascript/packs/20.js
+++ app/javascript/packs/20.js
@@ -5,13 +5,39 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = true; // show results straight away (otherwise one needs to wait until stop speaking)
 // recognition.continuous = true; // keeps on recognizing and returning results
 
-const listen = document.getElementById("record");
+const button = document.querySelector(".listen-button");
+const gap = document.querySelector(".gap");
+const message = document.querySelector(".message");
 
 let p = document.createElement('p');
 const words = document.querySelector(".words");
 words.appendChild(p);
 
+let listening = false;
+
 // functions
+
+function start() {
+  recognition.start();
+  button.classList.add("active");
+  gap.classList.add("texting");
+}
+
+function end() {
+  recognition.stop();
+  button.classList.remove("active");
+  gap.classList.remove("texting");
+}
+
+function toggleListening(eve) {
+  message.classList.add("hide");
+  listening ? end() : start();
+  listening = !listening;
+}
+
+function newParagraph() {
+  if (listening) recognition.start();
+}
 
 function speechText(e) {
   const transcript = Array.from(e.results)  // need to create an array from resultslist
@@ -28,11 +54,12 @@ function speechText(e) {
 
 // event listeners
 
-recognition.start();
+// recognition.start();
 
-// listen.addEventListener("click", window.recognition.start);
+
+button.addEventListener("click", (eve) => toggleListening());
 
 recognition.addEventListener("result", speechText);
 
-recognition.addEventListener("end", recognition.start);
+recognition.addEventListener("end", newParagraph);
 
