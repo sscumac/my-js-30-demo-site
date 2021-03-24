45b56be7f8519cb580bd45014e543a1397adf8a5
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-03-24 12:16:52 +0100

    first part

--- app/javascript/packs/22.js
+++ app/javascript/packs/22.js
@@ -5,3 +5,15 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector("[name='text']").value;  // .text is a property of the SpeechSynthesisUtterance interface
+
+function populateVoices() {
+  voices = this.getVoices();
+  console.log(voices);
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join("")
+  // console.log(voiceOptions);
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices); // speechSynthesis = global Variable ; voicechanged = event of Web Speech API
